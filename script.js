f5295901dc71b705d7a2f7a69937f6f08fae9d8c
Sofia0507 <sofiaiglesiasgarcia0@gmail.com>
2025-12-11 11:21:13 +0100

    carrousel

--- script.js
+++ script.js
@@ -21,3 +21,30 @@ function toggleInfo(id) {
   const info = document.getElementById('info-' + id);
   info.classList.toggle('visible');
 }
+
+
+
+
+// Pagina2
+
+const cards = document.querySelectorAll(".card");
+let activeIndex = 3;
+
+function update(){
+  cards.forEach((card, i) => {
+    card.style.setProperty("--i", i - activeIndex);
+    card.classList.toggle("active", i === activeIndex);
+  });
+}
+
+document.querySelector(".next").onclick = () => {
+  activeIndex = (activeIndex + 1) % cards.length;
+  update();
+};
+
+document.querySelector(".prev").onclick = () => {
+  activeIndex = (activeIndex - 1 + cards.length) % cards.length;
+  update();
+};
+
+
